17d0390540bf61a055e3e0a103c951ff5c189cd8
lipingcoding <lipingcoding@gmail.com>
2021-04-06 10:56:10 +0800

    back to IDGL

--- main.py
+++ main.py
@@ -15,16 +15,16 @@ from util import edge_index_to_sparse_tensor, Logger, mymkdir, nowdt
 
 
 @torch.no_grad()
-def test(model, raw_adj, normed_adj, x, y, y_onehot, train_mask, val_mask, test_mask, last_logits):
+def test(model, raw_adj, normed_adj, x, y, y_onehot, train_mask, val_mask, test_mask):
     model.eval()
     accs = []
     with torch.no_grad():
-        logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_one(last_logits, train_mask)
+        logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_one()
         first_adj = cur_normed_adj
         last_logits = logits.detach()
         
         for _ in range(args.max_iter):
-            logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_two(node_vec, last_logits, train_mask, first_adj)
+            logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_two(node_vec, first_adj)
 
         pred = logits
 
@@ -68,36 +68,30 @@ def train(dataset, train_mask, val_mask, test_mask, args):
         loss.backward()
         optimizer.step()
     
-    last_logits = pred.detach()
     for epoch in tqdm(range(args.epoch_pretrain, args.epoch)):
         if epoch == args.epoch_pretrain:
             print('\n**** Start to train LinBP ****\n')
         model.train()
 
 
-        logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_one(last_logits, train_mask)
+        logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_one()
         first_adj = cur_normed_adj
-        last_logits = logits.detach()
         
         loss1 = F.cross_entropy(logits[train_mask], y[train_mask])
 
         loss2 = 0
         for _ in range(args.max_iter):
-            logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_two(node_vec, last_logits, train_mask, first_adj)
+            logits, node_vec, cur_raw_adj, cur_normed_adj = model.forward_two(node_vec, first_adj)
             loss2 += F.cross_entropy(logits[train_mask], y[train_mask])
 
         loss = loss1 + loss2 / args.max_iter
 
-        # pred = model.forward(raw_adj, normed_adj, x, y_onehot, train_mask) 
-        reg_h_loss = torch.norm(model.H.sum(dim=1), p=1)
-        # loss = F.cross_entropy(pred[train_mask], y[train_mask]) + reg_h_loss
-        loss += reg_h_loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         accs = test(model, raw_adj, normed_adj, x, y,
-                    y_onehot, train_mask, val_mask, test_mask, last_logits)
+                    y_onehot, train_mask, val_mask, test_mask)
         if accs[1] > best_val_acc:
             best_val_acc = accs[1]
             choosed_test_acc = accs[2]
@@ -140,28 +134,18 @@ if __name__ == '__main__':
     parser.add_argument('--n_post_iter', type=int, default=1)
     parser.add_argument('--model', type=str, default='gcn')
 
-    parser.add_argument('--graph_learn', action='store_true', default=False)
     parser.add_argument('--mulH', action='store_true', default=False)
     parser.add_argument('--epsilon', type=float, default=0.)
     parser.add_argument('--num_pers', type=int, default=4)
-    # parser.add_argument('--eps_adj', type=float, default=4e-5)
     parser.add_argument('--max_iter', type=int, default=10)
     parser.add_argument('--skip_conn', type=float, default=0.8)
     parser.add_argument('--update_ratio', type=float, default=0.1)
 
-    parser.add_argument('--post', action='store_true')
+    parser.add_argument('--patience', type=int, default=100)
 
-    parser.add_argument('--patience', type=int, default=2000)
-
-    parser.add_argument('--H_ratio', type=float, default=0.5)
     
 
     args = parser.parse_args()
-
-    log_dir = 'log/cpgnn'
-    mymkdir(log_dir)
-    log_file_path = os.path.join(log_dir, f'{args.dataset}_cpgnn-{args.model}_log.txt')
-    sys.stdout = Logger(log_file_path)
 
     print(args)
 
--- model.py
+++ model.py
@@ -25,37 +25,24 @@ class CPGNN(nn.Module):
             self.belief_estimator = GCN(num_features, hidden, out_dim, args.dropout)
         else:
             raise NotImplementedError('Belief estimator not specified, MLP or ChebNet ?')
-        # self.linbp = LinBP(args.iterations, out_dim)
         self.n_post_iter = args.n_post_iter
         self.H = nn.Parameter(torch.zeros(out_dim, out_dim))
         self.H_inited = False
         self.args = args
         self.dataset = dataset
-    
-        if args.graph_learn:
-            self.graph_learner1 = GraphLearner(num_features, hidden,
-                                                 epsilon=args.epsilon,
-                                                 num_pers=args.num_pers)
+        self.graph_learner1 = GraphLearner(num_features, hidden,
+                                                epsilon=args.epsilon,
+                                                num_pers=args.num_pers)
 
-            self.graph_learner2 = GraphLearner(hidden,
+        self.graph_learner2 = GraphLearner(hidden,
                                                   out_dim,
                                                   epsilon=args.epsilon,
                                                  num_pers=args.num_pers)
 
-    def learn_graph(self, learner:GraphLearner, features, train_mask, mulH=False, logits=None, y_onehot=None):
+    def learn_graph(self, learner:GraphLearner, features):
 
         raw_adj = learner(features)
 
-        if mulH:
-            assert logits is not None
-            assert y_onehot is not None
-            pred = F.softmax(logits, dim=1)
-            y_train = pred * (1 - train_mask[:, None].float()) + y_onehot * train_mask[:, None]
-            H_ratio = self.args.H_ratio
-            H_coef = y_train @ self.H @ y_train.T
-            E_coef = torch.ones_like(H_coef, device=H_coef.device)
-            raw_adj = raw_adj * (H_ratio * H_coef + (1-H_ratio) * E_coef)
-        
         raw_adj = learner.build_epsilon_neighbourhood(raw_adj, markoff_value=0)
         adj = row_sum_one_normalize(raw_adj)
         adj = self.args.skip_conn * self.dataset['normed_adj'] + (1 - self.args.skip_conn) * adj
@@ -68,81 +55,31 @@ class CPGNN(nn.Module):
         """
         return self.belief_estimator(normed_adj, features)[0]
 
-    def forward(self, raw_adj, normed_adj, features, y_onehot, train_mask):
-        """
-        post_process
-        """
-        logits = self.belief_estimator(normed_adj, features)[0]
-        # post_belief, reg_h_loss = self.linbp(logits, raw_adj, y_onehot, train_mask)
-        if not self.H_inited:
-            self.init_H(raw_adj, y_onehot, logits, train_mask)
-        post_belief = self.post_process(raw_adj, logits, y_onehot)
-        return post_belief
 
-    def forward_one(self, logits, train_mask):
+    def forward_one(self):
         """
         First iteration for graph learn
         """
         dataset = self.dataset
         x = dataset['features']
-        # normed_adj = dataset['normed_adj'].cuda()
         raw_adj = dataset['raw_adj']
         y = dataset['labels']
         y_onehot = F.one_hot(y)
-        if self.args.mulH and not self.H_inited:
-            self.init_H(raw_adj, y_onehot, logits, train_mask)
-        
-        cur_raw_adj, cur_normed_adj = self.learn_graph(self.graph_learner1, x, train_mask, mulH=self.args.mulH, logits=logits, y_onehot=y_onehot)
+        cur_raw_adj, cur_normed_adj = self.learn_graph(self.graph_learner1, x)
 
         logits, node_vec = self.belief_estimator(cur_normed_adj, x)
 
-        if self.args.post:
-            logits = self.post_process(raw_adj, logits, y_onehot)
-
         return logits, node_vec, cur_raw_adj, cur_normed_adj
 
-    def forward_two(self, node_vec, logits, train_mask, first_adj):
+    def forward_two(self, node_vec, first_adj):
         dataset = self.dataset
         x = dataset['features']
-        # normed_adj = dataset['normed_adj'].cuda()
         raw_adj = dataset['raw_adj']
         y = dataset['labels'].cuda()
         y_onehot = F.one_hot(y)
 
-        if self.args.mulH:
-            assert self.H_inited
-
-        cur_raw_adj, cur_normed_adj = self.learn_graph(self.graph_learner2, node_vec, train_mask, mulH=self.args.mulH, logits=logits, y_onehot=y_onehot)
+        cur_raw_adj, cur_normed_adj = self.learn_graph(self.graph_learner2, node_vec)
         cur_normed_adj = self.args.update_ratio * cur_normed_adj + (1 - self.args.update_ratio) * first_adj
         logits, node_vec = self.belief_estimator(cur_normed_adj, x)
 
-        if self.args.post:
-            logits = self.post_process(raw_adj, logits, y_onehot)
-
-        return logits, node_vec, cur_raw_adj, cur_normed_adj
-        
-
-    def init_H(self, raw_adj, y_onehot, logits, train_mask):
-        assert not self.H_inited
-        print('Initing H...')
-        with torch.no_grad():
-            H_init = CompatibilityLayer.estimateH(
-                raw_adj, y_onehot, logits, train_mask)
-            H_init = CompatibilityLayer.makeSymmetricH(H_init)
-            H_init -= (1 / y_onehot.shape[1])
-            self.H.data = H_init
-        self.H_inited = True
-
-    def post_process(self, raw_adj, logits, y_onehot):
-        assert self.H_inited
-        prior_belief = F.softmax(logits, dim=1)  # eq 4
-        E_hat = prior_belief - (1 / y_onehot.shape[1])  # eq 5, E_hat is B^0
-        B_hat = E_hat
-
-        for _ in range(self.n_post_iter):
-            B_hat = E_hat + raw_adj @ (B_hat @ self.H)  # eq 6
-
-        # eq 7, accoring to open-sourced code, but different from the paper
-        post_belief = B_hat + (1 / y_onehot.shape[1])
-
-        return post_belief
+        return logits, node_vec, cur_raw_adj, cur_normed_adj        
